78c19b901d56092357190a99c63af7d7ff8ef650
JayBrown120 <43543960+JayBrown120@users.noreply.github.com>
2019-03-20 10:06:58 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+
+ $("#Goat").hide();
+
+
+$("button").click(function() {
+ $("#Goat").show();
+});
+
+
+
+$(".Goat3").click(function() {
+ $("#Goat3").show();
+});
+
+$(".Goat3").click(function() {
+ $("#Goat3").show();
+});
+
